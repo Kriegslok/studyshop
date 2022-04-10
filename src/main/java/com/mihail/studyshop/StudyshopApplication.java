04a593241fc16653d347ebfd484becd599f6f94c
Kriegslok <niors@mail.ru>
2022-04-10 15:36:58 +0300

    Initial commit

---
+++ src/main/java/com/mihail/studyshop/StudyshopApplication.java
@@ -0,0 +1,13 @@
+package com.mihail.studyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mihail/studyshop/StudyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihail.studyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
